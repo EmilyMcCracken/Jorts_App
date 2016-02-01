efac27227568f80daf98d001318cdb543618e5d4
Emily McCracken <emilymcc803@gmail.com>
2016-02-01 11:26:16 -0500

    created sessions and linked join table to user id and activity id

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,10 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'activities#index'
+  get 'login' => 'sessions#new', as: :login
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy', as: :logout
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
