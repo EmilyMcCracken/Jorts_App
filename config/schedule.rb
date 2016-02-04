074fd3d780fe6f11f9e1592df1fd5af4e5ca46a9
Dylan Weinberger <djweinbe@gmail.com>
2016-02-04 14:27:51 -0500

    Added Whenever Gem but it is not set up. Added a rake task to delete jobs after they are past due.

---
+++ config/schedule.rb
@@ -0,0 +1,29 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+set :output, "#{path}/cron_output.log"
+# job_type :rake,    "cd :path && :environment_variable=:environment bundle exec rake :task --silent :output"
+job_type :delete_act,  'cd :path && bin/rails runner -e development ":task" :output'
+job_type :delete_act,  '/Desktop/jorts/Jorts_App/lib/tasks'
+
+every :hour do # Many shortcuts available: :hour, :day, :month, :year, :reboot
+  rake ".ladeeda"
+end
